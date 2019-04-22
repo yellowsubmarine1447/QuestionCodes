24792c38ff1d6eae844e4199738dbeec85a837e7
TurtleMaster69420 <48506404+TurtleMaster69420@users.noreply.github.com>
2019-04-22 20:48:54 +1000

    Update overlapping_area.py

--- overlapping_area.py
+++ overlapping_area.py
@@ -1,10 +1,14 @@
-coords1 = input("Enter coordinates: ").split(",")
-coords2 = input("Enter coordinates: ").split(",")
-coords3 = input("Enter coordinates: ").split(",")
-coords4 = input("Enter coordinates: ").split(",")
+coords1 = input("Enter bottom left coordinates for rectangle 1: ").split(",")
+coords2 = input("Enter top right coordinates for rectangle 1: ").split(",")
+coords3 = input("Enter bottom left coordinates for rectangle 2: ").split(",")
+coords4 = input("Enter top right coordinates for rectangle 2: ").split(",")
+
+
 a, b = float(coords2[0]) - float(coords1[0]), float(coords2[1]) - float(coords1[1])
 c, d = float(coords3[0]) - float(coords1[0]), float(coords3[1]) - float(coords1[1])
 e, f = float(coords4[0]) - float(coords1[0]), float(coords4[1]) - float(coords1[1])
+
+
 if d <= 0:
     height = abs(f)
 if d > 0:
@@ -13,8 +17,12 @@ if c <= 0:
     length = abs(e)
 if c > 0:
     length = abs(a) - abs(c)
+    
+    
 if c >= a or d >= b or e <= 0 or f <= 0:
     area = 0
 else:
     area = length * height
+    
+    
 print(area)
